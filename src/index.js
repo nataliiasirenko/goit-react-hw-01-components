cb452e04d6f6aee6f530a748721e93b71f4c6798
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-11-10 13:56:00 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 
+
+// const elem1 =<span>Hello</span>;
+// const elem2 = <span>wold</span>
+
+// const jsxElement = (
+//   <div>
+//   {elem1}
+//   {elem2}
+//   </div>
+// );
+
+// console.log(jsxElement);
+
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
 
-const element = React.createElement(`div`, 
-{a:5, b:10, children: `hello world`});
 
-console.log(element);
+const imageUrl =
+  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const productPrice = 10.99;
 
-ReactDOM.render(element, document.querySelector(`#root`));
+const product = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="240" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {productPrice}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(product);
+
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
 // import './index.css';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
