218554910537f07a9b71664a9d5d366e559f07fc
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-11-11 15:28:23 +0000

    update data

--- src/index.js
+++ src/index.js
@@ -1,48 +1,14 @@
 
-
-// const elem1 =<span>Hello</span>;
-// const elem2 = <span>wold</span>
-
-// const jsxElement = (
-//   <div>
-//   {elem1}
-//   {elem2}
-//   </div>
-// );
-
-// console.log(jsxElement);
-
-
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-
-
-const imageUrl =
-  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
-const productPrice = 10.99;
-
-const product = (
-  <div>
-    <img src={imageUrl} alt="Tacos With Lime" width="240" />
-    <h2>Tacos With Lime</h2>
-    <p>Price: {productPrice}$</p>
-    <button type="button">Add to cart</button>
-  </div>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(product);
-
-
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
 // import './index.css';
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
+
+
